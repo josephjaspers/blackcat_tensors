f8c1b73b8f3f1cb9bc49fd2e1c6a21649ed24439
josephjaspers <jj804785@wcupa.edu>
2018-04-02 16:02:35 -0400

    Fixed mem-leak, revert part, Tensor_Core classes beautiful still

--- BlackCat_Tensors3.3/BC_Internals/BC_Core/Implementation_Core/Tensor_Core.h
+++ BlackCat_Tensors3.3/BC_Internals/BC_Core/Implementation_Core/Tensor_Core.h
@@ -36,9 +36,6 @@ struct Tensor_Core : Tensor_Core_Base<Tensor_Core<T>, _rankOf<T>>{
 	int* is = Mathlib::unified_initialize(is, DIMS());
 	int* os = Mathlib::unified_initialize(os, DIMS());
 
-	operator 	   scalar_type*()       { return array; }
-	operator const scalar_type*() const { return array; }
-
 	Tensor_Core() = default;
 	Tensor_Core(const Tensor_Core&) = default;
 	Tensor_Core(Tensor_Core&&) = default;
@@ -66,19 +63,6 @@ struct Tensor_Core : Tensor_Core_Base<Tensor_Core<T>, _rankOf<T>>{
 		}
 		Mathlib::initialize(array, this->size());
 	}
-	__BCinline__	   scalar_type& operator [] (int index) 	  { return DIMS() == 0 ? array[0] : array[index]; };
-	__BCinline__ const scalar_type& operator [] (int index) const { return DIMS() == 0 ? array[0] : array[index]; };
-
-	__BCinline__ int dims() const { return DIMS(); }
-	__BCinline__ int size() const { return DIMS() > 0 ? os[LAST()] : 1;    }
-	__BCinline__ int rows() const { return DIMS() > 0 ? is[0] : 1; }
-	__BCinline__ int cols() const { return DIMS() > 1 ? is[1] : 1; }
-	__BCinline__ int dimension(int i) const { return DIMS() > i ? is[i] : 1; }
-
-	__BCinline__ int LD_rows() const { return DIMS() > 0 ? os[0] : 1; }
-	__BCinline__ int LD_cols() const { return DIMS() > 1 ? os[1] : 1; }
-	__BCinline__ int LD_dimension(int i) const { return DIMS() > i + 1 ? os[i] : 1; }
-
 	__BCinline__ const auto innerShape() const { return is; }
 	__BCinline__ const auto outerShape() const { return os; }
 
@@ -86,21 +70,8 @@ struct Tensor_Core : Tensor_Core_Base<Tensor_Core<T>, _rankOf<T>>{
 	__BCinline__ const auto slice(int i) const { return slice_type(&array[slice_index(i)],*this); }
 	__BCinline__	   auto slice(int i) 	   { return slice_type(&array[slice_index(i)],*this); }
 
-	__BCinline__ const auto scalar(int i) const { static_assert (DIMS() != 0, "SCALAR OF SCALAR NOT DEFINED"); return Tensor_Scalar<self>(&array[i], *this); }
-	__BCinline__	   auto scalar(int i) 	    { static_assert (DIMS() != 0, "SCALAR OF SCALAR NOT DEFINED"); return Tensor_Scalar<self>(&array[i], *this); }
-	__BCinline__ const auto row(int i) const { static_assert (DIMS() != 2, "ROW OF NON-MATRIX NOT DEFINED"); return Tensor_Row<self>(&array[i], *this); }
-	__BCinline__	   auto row(int i) 	     { static_assert (DIMS() != 2, "ROW OF NON-MATRIX NOT DEFINED"); return Tensor_Row<self>(&array[i], *this); }
-	__BCinline__ const auto col(int i) const { static_assert (DIMS() != 2, "COL OF NON-MATRIX NOT DEFINED"); return slice(i); }
-	__BCinline__	   auto col(int i) 	     { static_assert (DIMS() != 2, "COL OF NON-MATRIX NOT DEFINED"); return slice(i); }
-
-	template<class... integers>  const auto reshape(integers... ints) const {
-		return Tensor_Reshape<const self, sizeof...(integers)>(*this, ints...); }
-	template<class... integers>   	   auto reshape(integers... ints)  		{
-		return Tensor_Reshape<self, sizeof...(integers)>(*this, ints...); }
-
 	const scalar_type* getIterator() const { return array; }
-		  scalar_type* getIterator()  	    { return array; }
-
+		  scalar_type* getIterator()  	   { return array; }
 
 	template<class... integers, int dim = 0>
 	void resetShape(integers... ints)  {
@@ -108,21 +79,6 @@ struct Tensor_Core : Tensor_Core_Base<Tensor_Core<T>, _rankOf<T>>{
 		Mathlib::destroy(array);
 		Mathlib::initialize(array, this->size());
 	}
-
-
-	void printDimensions() const {
-		for (int i = 0; i < DIMS(); ++i) {
-			std::cout << "[" << is[i] << "]";
-		}
-		std::cout << std::endl;
-	}
-	void printLDDimensions() const {
-		for (int i = 0; i < DIMS(); ++i) {
-			std::cout << "[" << os[i] << "]";
-		}
-		std::cout << std::endl;
-	}
-
 
 	int slice_index(int i) const {
 		if (DIMS() == 0)
--- BlackCat_Tensors3.3/BC_Internals/BC_Core/Implementation_Core/Tensor_Core_Scalar.h
+++ BlackCat_Tensors3.3/BC_Internals/BC_Core/Implementation_Core/Tensor_Core_Scalar.h
@@ -9,48 +9,27 @@
 #define TENSOR_Scalar_CU_
 
 #include "BC_Expressions/Expression_Base.h"
+#include "Tensor_Core_Interface.h"
 #include "Determiners.h"
 
 namespace BC {
 
 template<class PARENT>
-struct Tensor_Scalar : expression<_scalar<PARENT>, Tensor_Scalar<PARENT>> {
+struct Tensor_Scalar : Tensor_Core_Base<Tensor_Scalar<PARENT>, 0> {
 
 	using scalar = _scalar<PARENT>;
-	using self = Tensor_Scalar<PARENT>;
-	using Mathlib = typename  PARENT::Mathlib;
-
-
-	static constexpr int DIMS() { return 0; }
 
 	const PARENT parent;
 	scalar* array_slice;
 
-	operator 	   scalar*()       { return array_slice; }
-	operator const scalar*() const { return array_slice; }
+	__BCinline__ Tensor_Scalar(const scalar* array, const PARENT& parent_)
+		: array_slice(const_cast<scalar*>(array)), parent(parent_) {}
 
-	__BCinline__ Tensor_Scalar(scalar* array, const PARENT& parent_) : array_slice(array), parent(parent_) {}
-
-	__BCinline__ int dims() const { return 0; }
-	__BCinline__ int size() const { return 1; }
-	__BCinline__ int rows() const { return 1; }
-	__BCinline__ int cols() const { return 1; }
-	__BCinline__ int dimension(int i) const { return 1; }
-	__BCinline__ int LD_rows() const { return 0; }
-	__BCinline__ int LD_cols() const { return 0; }
-	__BCinline__ int LDdimension(int i) const { return 0; }
-	__BCinline__ const auto innerShape() const 			{ return parent.innerShape(); }
-	__BCinline__ const auto outerShape() const 			{ return parent.outerShape(); }
-
-	__BCinline__ const auto& operator [] (int i) const { return array_slice[0]; }
-	__BCinline__ 	   auto& operator [] (int i)  	   { return array_slice[0]; }
-
-	void printDimensions() 		const { parent.printDimensions(); }
-	void printLDDimensions()	const { parent.printDimensions(); }
+	__BCinline__ const auto innerShape() const 	{ return parent.innerShape(); }
+	__BCinline__ const auto outerShape() const 	{ return parent.outerShape(); }
 
 	const scalar* getIterator() const { return array_slice; }
-		  scalar* getIterator()  	   { return array_slice; }
-
+		  scalar* getIterator()  	  { return array_slice; }
 
 	};
 }
