114799a2490de0f0b2f68c498e35ac3a6fda2dd6
josephjaspers <jj804785@wcupa.edu>
2018-05-07 17:15:06 -0400

    optimization for parameter pack modifiers

--- BlackCat_Tensors3.3/BC_Internals/BC_Tensor_Derived/BC_Tensor_Base/BC_Tensor_Types/BC_Utility/ParameterPackModifiers.h
+++ BlackCat_Tensors3.3/BC_Internals/BC_Tensor_Derived/BC_Tensor_Base/BC_Tensor_Types/BC_Utility/ParameterPackModifiers.h
@@ -10,7 +10,7 @@
 
 /*
  * Code for altering parameter packs
- * --> In theory the compiler should be able to do this at compile time (as only the order of the parameters are being changed)
+ * --> Compiler elides these alterations (no run time cost)
  */
 
 namespace BC {
@@ -39,56 +39,16 @@ namespace PPack {
 	//removes head of a parameter pack and calls function f
 	template<class function, class front, class... set>
 	__BC_host_inline__ auto pop_head(function f, front first, set... pack) {
-
-		 std::vector<int> ds= {pack...};
-
-		 for (int i : ds) {
-			 std::cout << i << " ";
-		 }
-		 std::cout<<std::endl;
-
 		return f(pack...);
 	};
 
-//	//removes tail of parameter packs and calls function f
-//	template<class function, class... set>
-//	auto pop_tail(function f, set... params) {
-//		//read this bottom-up
-////
-//		//than re-reverses the list and calls the actual function
-//		 auto reverse_final = [&](auto&... xs) {
-//			return reverse(f, xs...);
-//		};
-//
-//		//then it removes the first element (which is the last element)
-//		 auto pop = [&](auto&... xs) {
-//			return pop_head(reverse_final, xs...);
-//		};
-//
-//		//first reverse the lists
-//		 auto reverse_initial = [&](auto&... xs) {
-//
-//			return reverse(pop, xs...);
-//		};
-//
-//		return reverse_initial(params...);
-//	};
+	template<class function, class... set>
+	auto pop_tail(function f, set... params) {
+		 auto pop = [&](auto&... xs) { return pop_head(f, xs...); };
+		 auto reverse_initial = [&](auto&... xs) { return reverse(pop, xs...); };
 
-	template<class function, class a, class b, class c, class d>
-	__BC_host_inline__ auto pop_tail(function f, a A,b B,c C,d) {
-		return f(A, B, C);
-
+		return reverse_initial(params...);
 	};
-	template<class function, class a, class b, class c>
-	__BC_host_inline__ auto pop_tail(function f, a A,b B,c) {
-		return f(A, B);
-
-	};
-	template<class function, class a, class b>
-	__BC_host_inline__ auto pop_tail(function f, a A, b) {
-		return f(A);
-	};
-
 	//returns tail of parameter pack
 	template<class last>
 	__BC_host_inline__ auto tail(last& l) -> decltype (l) {
