adb8629ec81e569792d7d0fd04de9588879837ec
joseph <jj804785@wcupa.edu>
2018-11-29 22:42:57 -0500

    initial benchmarks

---
+++ benchmarks/elementwise.h
@@ -0,0 +1,104 @@
+/*
+ * Elementwise.h
+ *
+ *  Created on: Oct 23, 2018
+ *      Author: joseph
+ */
+
+#ifndef ELEMENTWISE_H_
+#define ELEMENTWISE_H_
+#include <vector>
+#include <chrono>
+#include "../../BlackCat_Tensors/include/BlackCat_Tensors.h"
+
+
+
+auto benchmark_forloop(int size, int iters, bool stdout=false) {
+
+	using clock = std::chrono::duration<double>;
+
+    using vec = BC::Vector<double>;
+
+    clock bc_time;
+    clock cptr_time;
+
+    vec a(size);
+    vec b(size);
+    vec c(size);
+    vec d(size);
+    vec e(size);
+
+    b.randomize(-1000, 1000);
+    c.randomize(-1000, 1000);
+    d.randomize(-1000, 1000);
+    e.randomize(-1000, 1000);
+
+    double* a_ = a.data();
+    double* b_ = b.data();
+    double* c_ = c.data();
+    double* d_ = d.data();
+    double* e_ = e.data();
+
+
+
+    {
+        auto wcts = std::chrono::system_clock::now();
+
+        for (int i = 0; i < iters; ++i)
+            a = b + c - d / e;
+
+
+        bc_time = clock(std::chrono::system_clock::now() - wcts);
+        if (stdout)
+        std::cout << "BlackCat_Expression a = b + c - d / e \n" << bc_time.count() << " seconds [Wall Clock]\n\n" << std::endl;
+    }
+
+    {
+        auto wcts = std::chrono::system_clock::now();
+
+        for (int i = 0; i < iters; ++i)
+#pragma omp parallel for
+            for (int j = 0; j < size; ++j) {
+                a_[j] = b_[j] + c_[j] - d_[j] / e_[j];
+            }
+#pragma omp barrier
+        cptr_time = clock(std::chrono::system_clock::now() - wcts);
+        if (stdout)
+        std::cout << "For Loop  a = b + c - d / e \n" << cptr_time.count() << " seconds [Wall Clock]\n\n" << std::endl;
+
+    }
+
+    return std::make_pair(bc_time, cptr_time);
+
+}
+
+
+void benchmark_forloop_suite() {
+	int size = 1000000;
+	int reps = 100;
+	float multiplier = 1.5;
+
+	std::string markdown_header = {
+		"|Size | BC time | Baseline | Performance difference |\n" \
+		 "| --- | --- | --- | --- |"
+	};
+
+
+
+	std::cout << markdown_header << std::endl;
+	for (int i = 0; i < 10; ++i) {
+		auto times = benchmark_forloop(size, reps);
+		auto bc = times.first;
+		auto baseline = times.second;
+
+			std::cout << "|" << size \
+						<< "|" << bc.count() \
+					       << "|" << baseline.count() \
+					          <<"|"<< bc.count()/baseline.count() << "|" << std::endl;
+
+		size *= multiplier;
+	}
+}
+
+
+#endif /* ELEMENTWISE_H_ */
