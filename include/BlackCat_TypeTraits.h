d8f8b6eaa7ac87d118447289a968d38ec4b52d7f
josephjaspers <jj804785@wcupa.edu>
2019-04-19 18:16:26 -0400

    added BC::meta::constexpr_if, improve Optimizer implementation

--- include/BlackCat_TypeTraits.h
+++ include/BlackCat_TypeTraits.h
@@ -23,6 +23,9 @@ template<class... Ts> using void_t = void;
 	template<template<class> class func, class T>
 	struct is_detected<func, T, std::enable_if_t<std::is_void<void_t<func<T>>>::value>> : std::true_type { };
 
+	template<template<class> class func, class T>
+	static constexpr bool is_detected_v = is_detected<func, T>::value;
+
 	template<template<class> class func, class TestType, class DefaultType, class enabler=void>
 	struct conditional_detected {
 		using type = DefaultType;
@@ -31,19 +34,8 @@ template<class... Ts> using void_t = void;
 	struct conditional_detected<func, TestType, DefaultType, std::enable_if_t<is_detected<func,TestType>::value>> {
 		using type = func<TestType>;
 	};
-
-
-	/*
-	 * example -- has allocator --> template<class t> using allocator = typename allocator_t;
-	 *  --> is_detected<allocator, type_argument>::value
-	 *
-	 */
-
-	template<template<class> class func, class T>
-	static constexpr bool is_detected_v = is_detected<func, T>::value;
-
-
-    template<class t,class u> static constexpr bool is_same = std::is_same<t,u>::value;
+	template<template<class> class func, class TestType, class DefaultType>
+	using conditional_detected_t = typename conditional_detected<func, TestType, DefaultType>::type;
 
     BCINLINE static constexpr BC::size_t  max(int x) { return x; }
     BCINLINE static constexpr BC::size_t  min(int x) { return x; }
@@ -54,26 +46,18 @@ template<class... Ts> using void_t = void;
     template<class... integers>
     BCINLINE static constexpr BC::size_t  min(int x, integers... ints) { return x < min (ints...) ? x : min(ints...); }
 
-    static constexpr BC::size_t  sum(int x) { return x; }
-
-    template<class... integers>
-    static constexpr BC::size_t  sum(int x, integers... ints) { return x + sum(ints...); }
-
-    template<class T, class... Ts> using head_t = T;
-
     template<class T, class... Ts>
     struct seq_contains_impl {
         static constexpr bool value= false;
     };
     template<class T, class U, class... Ts>
-        struct seq_contains_impl<T,U,Ts...> {
-            static constexpr bool value= std::is_same<T,U>::value || seq_contains_impl<T,Ts...>::value;
-        };
+	struct seq_contains_impl<T,U,Ts...> {
+		static constexpr bool value= std::is_same<T,U>::value || seq_contains_impl<T,Ts...>::value;
+	};
 
     template<class T, class... Ts>
-    struct seq_of_impl {
-        static constexpr bool value = false;
-    };
+    struct seq_of_impl : std::false_type{};
+
     template<class T, class U>
     struct seq_of_impl<T,U> {
         static constexpr bool value = std::is_same<T,U>::value;
@@ -83,22 +67,18 @@ template<class... Ts> using void_t = void;
         static constexpr bool value = std::is_same<T,U>::value || seq_of_impl<T,Ts...>::value;
     };
 
-
-
     template<template<class> class function, class... Ts>
     struct seq_is : std::true_type {};
 
     template<template<class> class function, class T, class... Ts>
-        struct seq_is<function, T, Ts...> :
+	struct seq_is<function, T, Ts...> :
         std::conditional_t<function<T>::value && seq_is<function, Ts...>::value, std::true_type, std::false_type> {};
 
+    template<class T, class U, class... Ts>
+    static constexpr bool seq_of = seq_of_impl<T,U,Ts...>::value;
 
-
-
-
-    template<class T, class U, class... Ts> static constexpr bool seq_of = seq_of_impl<T,U,Ts...>::value;
-
-    template<class... Ts> static constexpr bool seq_contains = seq_contains_impl<Ts...>::value;
+    template<class... Ts> static constexpr bool
+    seq_contains = seq_contains_impl<Ts...>::value;
 
 
     template<int index, class... Ts>
@@ -155,9 +135,7 @@ template<class... Ts> using void_t = void;
     template<>
     struct constexpr_if_impl<false> {
         template<class f1>
-        static auto impl(f1 path) {
-            return path();
-        }
+        static void impl(f1 path) {}
     };
     template<bool b,class f>
     auto constexpr_if(f path) {
@@ -168,123 +146,88 @@ template<class... Ts> using void_t = void;
         return constexpr_ternary_impl<cond>::impl(true_path, false_path);
     }
 
-    template<bool, class...>
-    struct Else_If;
+    template<bool, class...> struct Constexpr_Else_If;
 
-    template<class function>
-    struct Else_If<false, function>  {
-		const function& f;
-		Else_If(const function& f_) : f(f_) { }
-		void operator () () const {}
-    };
-    template<class function>
-    struct Else_If<true, function>  {
-		function f;
-		Else_If(function f_) : f(f_) { }
+    template<class f1, class f2>
+    struct Constexpr_Else_If<true, f1, f2> {
+    	mutable f1 f1_;
+    	f2 f2_;
 
-		template<int ADL=0>
-		auto operator () () const {
-			return f();
-		}
-		template<int ADL=0>
-		auto operator () () {
-			return f();
-		}
-    };
-    template<class TruePath, class FalsePath>
-    struct Else_If<false, TruePath, FalsePath>  {
-    	TruePath f1;
-    	FalsePath f2;
-    	Else_If(TruePath f1_, FalsePath f2_)
-    		: f1(f1_), f2(f2_) {}
-
-		template<int ADL=0>
-		auto operator () () const -> decltype(f2()){
-			return f2();
-		}
-		template<int ADL=0>
-		auto operator () () -> decltype(f2()){
-			return f2();
-		}
-
-    };
-    template<class TruePath, class FalsePath>
-    struct Else_If<true, TruePath, FalsePath>  {
-    	mutable TruePath f1;
-    	mutable FalsePath f2;
-    	Else_If(TruePath f1_, FalsePath f2_)
-    		: f1(f1_), f2(f2_) {}
-
-		template<int ADL=0>
-		auto operator () () const {
-			return f1();
-		}
-		template<int ADL=0>
-		auto operator () ()  {
-			return f1();
-		}
-    };
-
-    template<bool cond, class... funcs>
-    auto else_if(funcs... fs) {
-    	return Else_If<cond,funcs...>(fs...);
-    }
-
-    //just for readability
-    template<class function>
-    auto else_(function f) {
-    	return f;
-    }
-
-    template<class function, class... args>
-    struct Bind {
-    	mutable function f;
-    	std::tuple<args...> m_args;
-
-    	Bind(function f_, args... args_): f(f_), m_args(args_...) {}
-
-    	template<int ADL=0>
-    	auto operator () () const {
-    		return call();
-    	}
     	template<int ADL=0>
     	auto operator () () {
-    		return call();
+    		return f1_();
     	}
-    private:
-    	auto call(args... args_) const {
-    		return f(args_...);
+    	template<int ADL=0>
+    	auto operator () () const {
+    		return f1_();
     	}
+    };
+    template<class f1, class f2>
+    struct Constexpr_Else_If<false, f1, f2> {
+    	f1 f1_;
+    	f2 f2_;
 
-    	template<class... xargs>
-    	auto call(xargs... args_)const {
-    		return call(args_..., std::get<sizeof...(xargs)>(m_args));
+    	template<int ADL=0>
+    	auto operator () () {
+    		return f2_();
+    	}
+    	template<int ADL=0>
+    	auto operator () () const {
+    		return f2_();
+    	}
+    };
+    template<class f1>
+    struct Constexpr_Else_If<true, f1> {
+    	f1 f1_;
+
+    	template<int ADL=0>
+    	auto operator () () {
+    		return f1_();
+    	}
+    	template<int ADL=0>
+    	auto operator () () const {
+    		return f1_();
+    	}
+    };
+    template<class f1>
+    struct Constexpr_Else_If<false, f1> {
+    	f1 f1_;
+
+    	template<int ADL=0>
+    	void operator () () {
+    	}
+    	template<int ADL=0>
+    	void operator () () const {
     	}
     };
 
-    template<class function, class... args>
-    Bind<function, args...> bind(function f, args... args_) {
-    	return  Bind<function,args...>(f, std::forward<args>(args_)...);
+    template<bool cond, class f1>
+    auto constexpr_else_if(f1 f1_) {
+    	return Constexpr_Else_If<cond, f1>{f1_};
+    }
+    template<bool cond, class f1, class f2>
+    auto constexpr_else_if(f1 f1_, f2 f2_) {
+    	return Constexpr_Else_If<cond, f1, f2>{f1_, f2_};
     }
 
-    template<class arg>
-    auto get_last(const arg& value) -> decltype(value) {
-    	return value;
-    }
-    template<class arg>
-	auto get_last(arg& value) -> decltype(value) {
-		return value;
-	}
+    template<class f1>
+    struct Else_ {
+    	f1 f1_;
 
-    template<class head, class... args>
-    auto get_last(head&, args&... args_) -> decltype(get_last(args_...)) {
-    	return get_last(args_...);
-    }
-    template<class head, class... args>
-	auto get_last(const head&, const args&... args_) -> decltype(get_last(args_...)) {
-		return get_last(args_...);
-	}
+    	template<int adl=0>
+    	auto operator () () {
+    		return f1_();
+    	}
+    	template<int adl=0>
+    	auto operator () () const {
+    		return f1_();
+    	}
+    };
 
+    template<class f1>
+    Else_<f1> else_(f1 f1_) {
+    	return Else_<f1>{f1_};
+    }
 
     template<class> struct DISABLE;
     template<bool x,class T> using only_if = std::conditional_t<x, T, DISABLE<T>>;
--- include/tensor/expression_templates/nd_evaluator/Device_Impl.cu
+++ include/tensor/expression_templates/nd_evaluator/Device_Impl.cu
@@ -90,45 +90,26 @@ template<typename T> __global__ static void eval5d(T t) {
     }
 }
 
+template<class T> __device__
+T& get(T& t) {
+	return t;
+}
+template<class T> __device__
+const T& get(const T& t) {
+	return t;
+}
+template<class T> __device__
+T& get(T* t) {
+	return *t;
+}
+
 template<class T, class U, class V> __global__
-static void scalar_mul(T* t, U* u, V* v) {
-    *t = *u * *v;
-}
-template<class T, class U, class V> __global__
-static void scalar_mul(T* t, U u, V* v) {
-    *t = u * *v;
-}
-template<class T, class U, class V> __global__
-static void scalar_mul(T* t, U* u, V v) {
-    *t = *u * v;
-}
-template<class T, class U, class V> __global__
-static void scalar_mul(T* t, U u, V v) {
-    *t = u * v;
-}
-
-template<class T> __global__
-static void randomize(T t, float lower_bound, float upper_bound, int seed) {
-
-     curandState_t state;
-      curand_init(seed, /* the seed controls the sequence of random values that are produced */
-                  seed, /* the sequence number is only important with multiple cores */
-                  1, /* the offset is how much extra we advance in the sequence for each call, can be 0 */
-                  &state);
-
-    constexpr int floating_point_decimal_length = 10000;
-
-    for (int i = 0; i < t.size(); ++i) {
-        t[i] = curand(&state) % floating_point_decimal_length;
-        t[i] /= floating_point_decimal_length;
-        t[i] *= (upper_bound - lower_bound);
-        t[i] += lower_bound;
-    }
-}
-
+static void scalar_mul(T t, U u, V v) {
+	get(t) = get(u) * get(v);
 }
 
 
+}
 }
 
 #endif
