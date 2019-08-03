0916fa46b2012bd90b005d8c3c7a920aeef3140e
josephjaspers <jj804785@wcupa.edu>
2019-08-03 14:56:20 -0400

    add correct delta-cell state

--- include/neural_networks/Layers/Recurrent.h
+++ include/neural_networks/Layers/Recurrent.h
@@ -28,20 +28,24 @@ struct Recurrent : public Layer_Base {
 
 private:
 
-    ValueType lr = 0.0003;
+    ValueType lr = 0.03;
 
-    mat w;  //weights
-    mat r;
-    vec b;  //biases
+    mat dc; //delta cell_state
+    mat w, w_gradients;  //weights
+    mat r, r_gradients;
+    vec b, b_gradients;  //biases
 
 public:
 
     Recurrent(int inputs, int outputs) :
         Layer_Base(inputs, outputs),
 		w(outputs, inputs),
+		w_gradients(outputs, inputs),
 		r(outputs, outputs),
-		b(outputs)
-    {
+		r_gradients(outputs, outputs),
+		b(outputs),
+		b_gradients(outputs)
+	{
         w.randomize(-2, 2);
         b.randomize(-2, 2);
     }
@@ -53,9 +57,10 @@ public:
 
     template<class X, class Delta>
     auto back_propagation(const X& x, const Delta& dy) {
-    	w -= lr * dy  * x.t();
-    	b -= lr * dy;
-    	return w.t() * dy;
+    	dc = dy;
+    	w_gradients -= lr * dc  * x.t();
+    	b_gradients -= lr * dc;
+    	return w.t() * dc;
     }
 
     template<class X, class Y>
@@ -65,10 +70,27 @@ public:
 
     template<class X, class Y, class Delta>
     auto back_propagation(const X& x, const Y& y, const Delta& dy) {
-    	w -= lr * dy  * x.t();
-    	b -= lr * dy;
-    	r -= lr * dy * y.t();
-    	return w.t() * dy;
+    	dc = dy + r.t() * dc;
+    	w_gradients -= lr * dc  * x.t();
+    	b_gradients -= lr * dc;
+    	r_gradients -= lr * dc * y.t();
+    	return w.t() * dc;
+    }
+
+    void update_weights() {
+    	w += w_gradients / this->batch_size();
+    	b += b_gradients / this->batch_size();
+    	r += r_gradients / this->batch_size();
+
+    	w_gradients.zero();
+    	b_gradients.zero();
+    	r_gradients.zero();
+    	dc.zero();
+    }
+
+    void set_batch_size(BC::size_t bs) {
+    	Layer_Base::set_batch_size(bs);
+    	dc = mat(this->output_size(), bs);
     }
 };
 
