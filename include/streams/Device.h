364e627e4afa36cb2da78b80dbd284b60457eb62
joseph <jj804785@wcupa.edu>
2019-12-02 19:37:29 -0500

    initial

--- include/streams/Device.h
+++ include/streams/Device.h
@@ -29,10 +29,10 @@ class Stream<device_tag> {
 
 	struct Device_Stream_Contents {
 
-		HostStream	   m_host_stream;
+		HostStream     m_host_stream;
 		cublasHandle_t m_cublas_handle;
 		cudaStream_t   m_stream_handle=nullptr;
-		cudaEvent_t	m_event_handle=nullptr;
+		cudaEvent_t    m_event_handle=nullptr;
 
 		BC::allocators::Stack_Allocator<device_tag> m_workspace;
 
@@ -56,13 +56,15 @@ class Stream<device_tag> {
 		 }
 	};
 
-	static std::shared_ptr<Device_Stream_Contents> get_default_contents() {
-		thread_local std::shared_ptr<Device_Stream_Contents> default_contents =
-				std::shared_ptr<Device_Stream_Contents>(new Device_Stream_Contents(false));
+	using contents_handle_t = std::shared_ptr<Device_Stream_Contents>;
+
+	static contents_handle_t get_default_contents() {
+		thread_local contents_handle_t default_contents =
+				contents_handle_t(new Device_Stream_Contents(false));
 		return default_contents;
 	}
 
-	std::shared_ptr<Device_Stream_Contents> device_contents = get_default_contents();
+	contents_handle_t m_contents = get_default_contents();
 
 public:
 
@@ -70,7 +72,7 @@ public:
 	using allocator_type = BC::allocators::Stack_Allocator<device_tag>;
 
 	BC::allocators::Stack_Allocator<device_tag>& get_allocator() {
-		return device_contents.get()->m_workspace;
+		return m_contents->m_workspace;
 	}
 
 	template<class RebindType>
@@ -79,75 +81,70 @@ public:
 	}
 
 	auto set_blas_pointer_mode_host() {
-		cublasSetPointerMode(device_contents.get()->m_cublas_handle, CUBLAS_POINTER_MODE_HOST);
+		cublasSetPointerMode(m_contents->m_cublas_handle, CUBLAS_POINTER_MODE_HOST);
 	}
 	auto set_blas_pointer_mode_device() {
-		cublasSetPointerMode(device_contents.get()->m_cublas_handle, CUBLAS_POINTER_MODE_DEVICE);
+		cublasSetPointerMode(m_contents->m_cublas_handle, CUBLAS_POINTER_MODE_DEVICE);
 	}
 
-	const cublasHandle_t& get_cublas_handle() const {
-		return device_contents.get()->m_cublas_handle;
+	cublasHandle_t get_cublas_handle() const {
+		return m_contents->m_cublas_handle;
 	}
 
-	cublasHandle_t& get_cublas_handle() {
-		return device_contents.get()->m_cublas_handle;
+	operator cudaStream_t() const {
+		return m_contents->m_stream_handle;
 	}
-
-
-   operator cudaStream_t() const {
-		return device_contents.get()->m_stream_handle;
-	}
-
 
 	void set_stream(Stream dev) {
-		device_contents = dev.device_contents;
+		m_contents = dev.m_contents;
 	}
 
 	void record_event() {
 		BC_CUDA_ASSERT(cudaEventRecord(
-				device_contents.get()->m_event_handle,
-				device_contents.get()->m_stream_handle));
+				m_contents->m_event_handle,
+				m_contents->m_stream_handle));
 	}
 
 	void wait_event(Stream& stream) {
 		BC_CUDA_ASSERT(cudaStreamWaitEvent(
-				device_contents.get()->m_stream_handle,
-				stream.device_contents.get()->m_event_handle, 0));
+				m_contents->m_stream_handle,
+				stream.m_contents->m_event_handle, 0));
 	}
 
 	void wait_stream(Stream& stream) {
 		stream.record_event();
 		BC_CUDA_ASSERT(cudaStreamWaitEvent(
-				device_contents.get()->m_stream_handle,
-				stream.device_contents.get()->m_event_handle, 0));
+				m_contents->m_stream_handle,
+				stream.m_contents->m_event_handle, 0));
 	}
 
 	void wait_event(cudaEvent_t event) {
 		BC_CUDA_ASSERT(cudaStreamWaitEvent(
-				device_contents.get()->m_stream_handle,
+				m_contents->m_stream_handle,
 				event, 0));
 	}
 
 	bool is_default() {
-		return device_contents.get()->m_stream_handle == 0;
+		return m_contents->m_stream_handle == 0;
 	}
 
 	void create() {
-		device_contents = std::shared_ptr<Device_Stream_Contents>(
-				new Device_Stream_Contents(true));
+		m_contents =
+				contents_handle_t(new Device_Stream_Contents(true));
 	}
+
 	void destroy() {
-		//'reset' to default
-		device_contents = get_default_contents();
+		m_contents = get_default_contents();
 	}
 
-	void sync() {
-		if (!is_default())
-			BC_CUDA_ASSERT(cudaStreamSynchronize(device_contents.get()->m_stream_handle));
+	void sync()
+	{
+		if (!is_default()) {
+			BC_CUDA_ASSERT(
+					cudaStreamSynchronize(m_contents->m_stream_handle));
+		}
 	}
 
-	//Functions (even functions using the internal stream)
-	//should use 'enqueue'
 	template<class Function>
 	void enqueue(Function f) {
 		f();
@@ -155,29 +152,36 @@ public:
 
 	template<
 		class function,
-		class=std::enable_if_t<std::is_void<decltype(std::declval<function>()())>::value>>
-	void enqueue_callback(function func) {
-
-		if (is_default()){
+		class=std::enable_if_t<
+				std::is_void<
+						decltype(std::declval<function>()())>::value>>
+	void enqueue_callback(function func)
+	{
+		if (is_default()) {
 			func();
 			return;
 		}
 
 		BC_CUDA_ASSERT(cudaEventRecord(
-				device_contents.get()->m_event_handle,
-				device_contents.get()->m_stream_handle));
+				m_contents->m_event_handle,
+				m_contents->m_stream_handle));
 
-		device_contents.get()->m_host_stream.push(
+		m_contents->m_host_stream.push(
 				[&, func]() {
-					cudaEventSynchronize(device_contents.get()->m_event_handle);
+					cudaEventSynchronize(m_contents->m_event_handle);
 					func();
 				}
 		);
 	}
+
 	template<
 		class function,
-		class=std::enable_if_t<!std::is_void<decltype(std::declval<function>()())>::value>, int ADL=0>
-	auto enqueue_callback(function func) {
+		class=std::enable_if_t<
+				!std::is_void<
+						decltype(std::declval<function>()())>::value>,
+		int ADL=0>
+	auto enqueue_callback(function func)
+	{
 		std::promise<decltype(func())> promise;
 
 		if (is_default()){
@@ -185,28 +189,27 @@ public:
 			return promise.get_future();
 		}
 
-
 		auto future = promise.get_future();
 		BC_CUDA_ASSERT(cudaEventRecord(
-				device_contents.get()->m_event_handle,
-				device_contents.get()->m_stream_handle));
+				m_contents->m_event_handle,
+				m_contents->m_stream_handle));
 
-		device_contents.get()->m_host_stream.push(
+		m_contents->m_host_stream.push(
 				BC::traits::bind(
 				[this, func](std::promise<decltype(func())> promise) {
-					cudaEventSynchronize(this->device_contents.get()->m_event_handle);
+					cudaEventSynchronize(this->m_contents->m_event_handle);
 					promise.set_value(func());
-		}, std::move(promise))
-		);
+		}, std::move(promise)));
+
 		return future;
 	}
 
 	bool operator == (const Stream& dev) {
-		return device_contents == dev.device_contents;
+		return m_contents == dev.m_contents;
 	}
 
 	bool operator != (const Stream& dev) {
-		return device_contents != dev.device_contents;
+		return m_contents != dev.m_contents;
 	}
 };
 
