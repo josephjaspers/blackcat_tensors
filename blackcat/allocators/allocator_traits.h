6e7ee0709c866f84cddf391f79aa6b259b58d59a
josephjaspers <jj804785@wcupa.edu>
2020-02-01 14:27:37 -0500

    deprecate is_managed_memory from allocator_traits

--- blackcat/allocators/allocator_traits.h
+++ blackcat/allocators/allocator_traits.h
@@ -13,31 +13,17 @@
 namespace bc {
 
 class host_tag;
-class device_tag;
 
 namespace allocators {
-namespace detail {
-
-	template<class T>
-	using query_managed_memory = bc::traits::truth_type<T::managed_memory>;
-
-}
 
 template<class Allocator>
 struct allocator_traits : std::allocator_traits<Allocator>
 {
 	using system_tag = bc::traits::conditional_detected_t<
 		bc::traits::query_system_tag, Allocator, host_tag>;
-
-	using is_managed_memory_t = bc::traits::conditional_detected_t<
-		detail::query_managed_memory, Allocator, std::false_type>;
-
-	static constexpr bool is_managed_memory = is_managed_memory_t::value;
 };
 
 }
 }
-
-
 
 #endif /* ALLOCATOR_TRAITS_H_ */
